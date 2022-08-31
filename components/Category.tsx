9af20b94cad8524a64bdd80e37a0ea4b8907772c
Marius Modlich <marius@modlich.de>
2022-08-31 20:24:42 +0200

    Fixed some SEO...

--- components/Category.tsx
+++ components/Category.tsx
@@ -12,7 +12,7 @@ export default function Category({ setSelectedTag, tag, selectedTag }) {
       onClick={() => handleTagClick(tag)}
       className={`${
         selectedTag === tag && 'ring-2 ring-catred-500 text-catred-300 font-bold'
-      } inline-flex items-center px-5 py-2.5 drop-shadow-lg uppercase bg-blue-700 text-offwhite-1 rounded-xl cursor-pointer font-heading text-xl`}
+      } inline-flex items-center px-5 py-2.5 drop-shadow-lg uppercase bg-blue-700 text-offwhite-1 hover:shadow-xl rounded-xl cursor-pointer font-heading text-xl`}
     >
       <span className="text-xs font-medium uppercase">{tag || 'All'}</span>
     </div>
--- layouts/MetaHead.tsx
+++ layouts/MetaHead.tsx
@@ -2,7 +2,7 @@ import siteData from 'data/siteData';
 import Head from 'next/head';
 
 export function MetaHead(props) {
-  const { date, title, imageUrl, description, ogUrl } = props;
+  const { date, title, description, ogUrl } = props;
 
   const titleName = title || siteData.title;
 
@@ -10,24 +10,24 @@ export function MetaHead(props) {
     <Head>
       <title>{titleName}</title>
       <meta name="robots" content="follow, index" />
-      <meta content={description || titleName} name="description" />
+      <meta content={description} name="description" />
       <meta property="og:site_name" content={siteData.author} />
       <meta property="og:description" content={description} />
       <meta property="og:title" content={siteData.title} />
-      <meta property="og:image" content={imageUrl || siteData.socialBanner} />
+      <meta property="og:image" content={siteData.socialBanner} />
 
       <meta property="og:url" content={ogUrl} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={titleName} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={imageUrl} />
+      <meta property="og:image" content={siteData.socialBanner} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content={siteData.websiteUrl} />
       <meta property="twitter:url" content={ogUrl} />
       <meta name="twitter:title" content={titleName} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={imageUrl} />
+      <meta name="twitter:image" content={siteData.socialBanner} />
       {date && <meta property="article:published_time" content={date} />}
     </Head>
   );
